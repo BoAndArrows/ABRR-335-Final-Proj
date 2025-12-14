497d040d66e40af649fb4108dac8ce95f16b3262
BoAndArrows <bocheung15@gmail.com>
2025-12-14 16:06:43 -0500

    Setting up render serviece

--- model/placeholder.js
+++ model/placeholder.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"));
+
+app.get("/", async (req, res) =>{
+    res.render('placeholder');
+})

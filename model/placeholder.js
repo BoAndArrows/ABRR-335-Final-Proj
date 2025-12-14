89b9ec15b972bbff377c5ada6b00993c7af1cb25
BoAndArrows <bocheung15@gmail.com>
2025-12-14 16:16:24 -0500

    body parser needed by render

--- model/placeholder.js
+++ model/placeholder.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");

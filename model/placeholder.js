347332f1a9908b361d02261384f64c359bd1f813
BoAndArrows <bocheung15@gmail.com>
2025-12-14 16:27:27 -0500

    new port number and console.log render link

--- model/placeholder.js
+++ model/placeholder.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const path = require("path");
+const portNumber = process.env.PORT || 7003;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -12,3 +13,6 @@ app.set("views", path.resolve(__dirname, "templates"));
 app.get("/", async (req, res) => {
     res.render("placeholder");
 });
+
+app.listen(portNumber);
+console.log(`main URL http://localhost:${portNumber}/`);

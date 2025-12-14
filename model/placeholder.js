7142021a8e7460f6d954ac05eaa0303321da5422
BoAndArrows <bocheung15@gmail.com>
2025-12-14 16:19:59 -0500

    no body parser test

--- model/placeholder.js
+++ model/placeholder.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const path = require("path");
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
 
-app.get("/", async (req, res) =>{
-    res.render('placeholder');
-})
+app.get("/", async (req, res) => {
+    res.render("placeholder");
+});
